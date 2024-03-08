73da0682dceb5c78724f12cbd261053ea30cce0c
anil kumar sahu <anildsvv14@gmail.com>
2024-03-08 10:06:26 +0530

    Array Reverse Using a Loop (In-place):

--- Array2.java
+++ Array2.java
@@ -2,7 +2,7 @@
 
 //1. Array Reverse Using an Extra Array (Non In-place):
  class ReverseArrayExtraArray {
-    public static void reverseArrayExtraArray(int[] arr) {
+    /*public static void reverseArrayExtraArray(int[] arr) {
         int[] reversedArr = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             reversedArr[i] = arr[arr.length - i - 1];
@@ -13,13 +13,43 @@
         for (int i : reversedArr) {
             System.out.print(i + " ");
         }
-    }
+    }*/
+    static void rvereseArray(int arr[], 
+            int start, int end) 
+        { 
+         int temp; 
+
+        while (start < end) 
+        { 
+            temp = arr[start]; 
+            arr[start] = arr[end]; 
+            arr[end] = temp; 
+            start++; 
+            end--; 
+        } 
+        }     
+
+        /* Utility that prints out an 
+        array on a line */
+        static void printArray(int arr[], 
+                    int size) 
+        { 
+        for (int i = 0; i < size; i++) 
+        System.out.print(arr[i] + " "); 
+
+        System.out.println(); 
+        } 
 }
 
 public class Array2{
     public static void main(String[] args) {
         ReverseArrayExtraArray test1=new ReverseArrayExtraArray();
-        int[] originalArr = {1, 2, 3, 4, 5};
-        test1.reverseArrayExtraArray(originalArr);
+        //int[] originalArr = {1, 2, 3, 4, 5};
+        //test1.reverseArrayExtraArray(originalArr);
+        int arr[] = {1, 2, 3, 4, 5, 6}; 
+        test1.printArray(arr, 6); 
+        test1.rvereseArray(arr, 0, 5); 
+        System.out.print("Reversed array is \n"); 
+        test1.printArray(arr, 6); 
     }
 }
