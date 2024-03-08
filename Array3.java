f6b4e34f4c1bc03fdfdc7e6d07e26e998e1cbd50
anil kumar sahu <anildsvv14@gmail.com>
2024-03-08 10:10:03 +0530

    //Array Reverse Inbuilt Methods

---
+++ Array3.java
@@ -0,0 +1,18 @@
+//Array Reverse Inbuilt Methods
+
+import java.util.Arrays;
+
+public class Array3 {
+    public static void main(String[] args) {
+        int[] originalArray = {1, 2, 3, 4, 5};
+ 
+        // Using inbuilt method in Java
+        int[] reversedArray = new int[originalArray.length];
+        for (int i = 0; i < originalArray.length; i++) {
+            reversedArray[i] = originalArray[originalArray.length - 1 - i];
+        }
+ 
+        // Print the reversed array
+        System.out.println(Arrays.toString(reversedArray));
+    }
+}
